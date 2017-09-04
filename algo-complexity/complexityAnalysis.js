a7035c1c42a6820ee38a69b2035c542eca79ef1a
maimai84 <maialmasalmah@gmail.com>
2017-09-04 09:07:27 +0300

    hehe

--- algo-complexity/complexityAnalysis.js
+++ algo-complexity/complexityAnalysis.js
@@ -2,5 +2,9 @@
 // Be as clear and complete as necessary to demonstrate your mastery.
 // Write your explanation below as a comment
 /*
+O(1) constant no time complexity
+O(n)  complexity there is  one loop 
+log(n) there is tree or book we search in half of the book
+O(n2) there is two for loop 
 
 */
